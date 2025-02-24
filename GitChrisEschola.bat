fe64f1c99c9f53b5a6ad4290287d8dfbbf2b2227
ChriSM <160172479+Chris-SM@users.noreply.github.com>
2025-02-24 12:33:13 -0300

    Senha adiciona no GitChrisEschola.bat

--- GitChrisEschola.bat
+++ GitChrisEschola.bat
@@ -1,5 +1,8 @@
 REM "Definir o proxy da escola"
 git config --global http.proxy http://etecadolphoberezin@17.1.0.1:3128
+REM "Senha da Rede"
+git config --global credential.provider generic
+
 
 REM definir minha conta
 git config --global user.name "Chris-SM"
